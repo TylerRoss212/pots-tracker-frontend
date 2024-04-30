f28e27d963a27bca1e62e07be6cbb75425a7fcde
TylerRoss212 <Tyler_Ross3@alumni.baylor.edu>
2024-04-30 15:00:54 -0500

    showing secret on page :/ i am running out of ideas

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,8 @@ export default function About() {
         <div>
             <Navbar></Navbar>
             <p className="extend">This is an app created to track fainting episodes caused by POTS.</p>
+            <p>{process.env.GOOGLE_CLIENT_ID}</p>
+            <p>{process.env.GOOGLE_CLIENT_SECRET}</p>
         </div>
     );
 }
