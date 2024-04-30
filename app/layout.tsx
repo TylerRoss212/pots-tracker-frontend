6a25af0a6cef65f849749f607f2b310c8110d46d
TylerRoss212 <Tyler_Ross3@alumni.baylor.edu>
2024-04-29 21:57:24 -0500

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Sono } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const font = Sono({ subsets: ["latin"] });
