ae535dbe03da995fa4954b3bdddee932580faea1
TylerRoss212 <Tyler_Ross3@alumni.baylor.edu>
2024-05-02 12:18:37 -0500

    Added edit functionality

--- app/my-times/time_functions.ts
+++ app/my-times/time_functions.ts
@@ -25,3 +25,46 @@ export async function deleteTime(id: number) {
     console.log("Sending to delete-time");
     await fetch((process.env.API_URL + "/delete-time"), options);
 }
+
+export async function updateTime(time_id: number, fields: {minutes: string, seconds: string, milliseconds: string, comment: string} ) {
+    const paddedMinutes = padWithZeros(fields.minutes, 2);
+    const paddedSeconds = padWithZeros(fields.seconds, 2);
+    const paddedMilliseconds = padWithZeros(fields.milliseconds, 2);
+
+    // update time
+    let options = awsSignRequests({
+        credentials: {
+            access_key: process.env.ACCESS_KEY_ID,
+            secret_key: process.env.SECRETKEY
+        },
+        method: "POST",
+        url: (process.env.API_URL + "/update-time"),
+        service_info: {
+            service: "execute-api",
+            region: "us-east-2"
+        },
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            time_id: time_id,
+            time: (paddedMinutes + paddedSeconds + paddedMilliseconds),
+            comment: fields.comment
+        }
+    });
+
+    console.log("Sending to update-time");
+    await fetch((process.env.API_URL + "/update-time"), options);
+}
+
+function padWithZeros(value: string, length: number): string {
+    // Ensure the value is a string
+    let paddedValue = String(value);
+
+    // Add zeros to the front until the length is reached
+    while (paddedValue.length < length) {
+        paddedValue = '0' + paddedValue;
+    }
+
+    return paddedValue;
+}
--- app/my-times/time_table.tsx
+++ app/my-times/time_table.tsx
@@ -1,34 +1,60 @@
 "use client"
-import { useEffect, useState } from "react";
-import { deleteTime } from "./time_functions";
+import { useEffect, useState, useRef } from "react";
+import { deleteTime, updateTime } from "./time_functions";
 
 import "./time_table.css";
 
 export default function TimeTable({ times }: { times: any }) {
     const [editingTimeId, setEditingTimeId] = useState<number | null>(null);
+    const [isEditing, setIsEditing] = useState<boolean>(false);
     let popupWrapper: HTMLElement | null;
 
     const dateOptions = {
         year: 'numeric',
-        month: 'numeric', 
+        month: 'numeric',
         day: 'numeric',
         hour: 'numeric',
         minute: 'numeric',
         second: 'numeric',
-        hour12: true, // Use 12-hour format with AM/PM
-        timeZone: 'UTC' // Set the timezone to UTC to display date consistently
+        hour12: true,
+        timeZone: 'UTC'
     }
 
     useEffect(() => {
         popupWrapper = document.getElementById("popup-wrapper");
     });
 
-    function allowEdit(timeId: number) {
-        setEditingTimeId(timeId);
+    // Declare refs for minutes, seconds, milliseconds, and comment
+    const minutesRef = useRef<HTMLSpanElement>(null);
+    const secondsRef = useRef<HTMLSpanElement>(null);
+    const millisecondsRef = useRef<HTMLSpanElement>(null);
+    const commentRef = useRef<HTMLTableCellElement>(null);
+
+    async function handleSave() {
+        // Fetch edited values for minutes, seconds, milliseconds, and comment
+        const minutes = minutesRef.current?.innerText;
+        const seconds = secondsRef.current?.innerText;
+        const milliseconds = millisecondsRef.current?.innerText;
+        const comment = commentRef.current?.innerText;
+
+        // If any of the values are missing, do not proceed
+        if (!minutes || !seconds || !milliseconds || !comment || !editingTimeId) {
+            console.error("One or more elements or editing time ID are missing references.");
+            return;
+        }
+
+        // Perform update with the obtained data
+        await updateTime(editingTimeId, { minutes, seconds, milliseconds, comment });
+
+        // Reset editing state
+        setEditingTimeId(null);
+        setIsEditing(false);
+        location.reload();
     }
 
-    function handleSave() {
-        setEditingTimeId(null);
+    function allowEdit(timeId: number) {
+        setEditingTimeId(timeId);
+        setIsEditing(true);
     }
 
     function handleDelete() {
@@ -46,8 +72,10 @@ export default function TimeTable({ times }: { times: any }) {
 
     function handleCancel() {
         setEditingTimeId(null);
+        setIsEditing(false);
+        location.reload();
     }
-    
+
     return (
         <table className="table">
             <thead>
@@ -66,9 +94,10 @@ export default function TimeTable({ times }: { times: any }) {
                             {Intl.DateTimeFormat('en-US', dateOptions).format(new Date(time.date_time))}
                         </td>
                         <td>
-                        <TimeFormat time={time.time}></TimeFormat>
+                            <TimeFormat time={time.time} isEditing={isEditing && editingTimeId === time.time_id} minutesRef={minutesRef} secondsRef={secondsRef} millisecondsRef={millisecondsRef}></TimeFormat>
                         </td>
-                        <td>{time.comment}</td>
+                        {/* Assign ref only when editing */}
+                        <td contentEditable={isEditing && editingTimeId === time.time_id} ref={isEditing && editingTimeId === time.time_id ? commentRef : null}>{time.comment}</td>
                         {
                             editingTimeId === time.time_id ? (
                                 <td>
@@ -80,10 +109,10 @@ export default function TimeTable({ times }: { times: any }) {
                                             <div className="popup">
                                                 <div className="popup-confirmation">Are you sure you want to delete this time?</div>
                                                 <div className="popup-date">
-                                                    {/* @ts-ignore */ }
-                                                    {Intl.DateTimeFormat('en-US', dateOptions).format(new Date(time.date_time))}    
+                                                    {/* @ts-ignore */}
+                                                    {Intl.DateTimeFormat('en-US', dateOptions).format(new Date(time.date_time))}
                                                 </div>
-                                                <TimeFormat time={time.time}></TimeFormat>
+                                                <TimeFormat time={time.time} isEditing={false} minutesRef={minutesRef} secondsRef={secondsRef} millisecondsRef={millisecondsRef}></TimeFormat>
                                                 <div className="bottom-buttons">
                                                     <button type="button" className="popup-button" onClick={cancelDelete}>CANCEL</button>
                                                     <button type="submit" className="popup-button" onClick={() => { confirmDelete(time.time_id) }}>DELETE</button>
@@ -92,7 +121,7 @@ export default function TimeTable({ times }: { times: any }) {
                                         </div>
                                     </div>
                                 </td>
-                            ) : ( 
+                            ) : (
                                 <td>
                                     <button onClick={() => allowEdit(time.time_id)}>EDIT</button>
                                 </td>
@@ -105,18 +134,69 @@ export default function TimeTable({ times }: { times: any }) {
     );
 }
 
-function TimeFormat({ time }: { time: string} ) {
+function TimeFormat({ time, isEditing, minutesRef, secondsRef, millisecondsRef }: { time: string, isEditing: boolean, minutesRef: React.RefObject<HTMLSpanElement>, secondsRef: React.RefObject<HTMLSpanElement>, millisecondsRef: React.RefObject<HTMLSpanElement> }) {
     let minutes = time.substring(0, 2);
     let seconds = time.substring(2, 4);
     let millisecons = time.substring(4, 6);
 
+    function validateNumberInput(event: any) {
+        let text = event.target.innerText.trim();
+
+        text = text.replace(/[^\d]/g, '');
+        text = text.slice(0, 2);
+
+        const selection = window.getSelection();
+        const range = selection!.getRangeAt(0);
+        const clonedRange = range.cloneRange();
+        clonedRange.selectNodeContents(event.target);
+        clonedRange.setEnd(range.endContainer, range.endOffset);
+
+        const cursorPosition = clonedRange.toString().length;
+
+        event.target.innerText = text;
+
+        const newRange = createRange(event.target, cursorPosition);
+        selection?.removeAllRanges();
+        selection?.addRange(newRange);
+        newRange.collapse();
+    }
+
     return (
         <div>
-            <span>{minutes}</span>
+            {/* Assign ref only when editing */}
+            <span className="time-field" contentEditable={isEditing} onInput={validateNumberInput} ref={isEditing ? minutesRef : null}>{minutes}</span>
             <span>:</span>
-            <span>{seconds}</span>
+            <span className="time-field" contentEditable={isEditing} onInput={validateNumberInput} ref={isEditing ? secondsRef : null}>{seconds}</span>
             <span>:</span>
-            <span>{millisecons}</span>
+            <span className="time-field" contentEditable={isEditing} onInput={validateNumberInput} ref={isEditing ? millisecondsRef : null}>{millisecons}</span>
         </div>
     );
 }
+
+const createRange = (node: Node, targetPosition: number) => {
+    let range = document.createRange();
+    range.selectNode(node);
+    range.setStart(node, 0);
+
+    let pos = 0;
+    const stack = [node];
+    while (stack.length > 0) {
+        const current = stack.pop();
+
+        if (current!.nodeType === Node.TEXT_NODE) {
+            const len = current!.textContent!.length;
+            if (pos + len >= targetPosition) {
+                range.setEnd(current!, targetPosition - pos);
+                return range;
+            }
+            pos += len;
+        } else if (current!.childNodes && current!.childNodes.length > 0) {
+            for (let i = current!.childNodes.length - 1; i >= 0; i--) {
+                stack.push(current!.childNodes[i]);
+            }
+        }
+    }
+
+    range.setEnd(node, node.childNodes.length);
+    return range;
+};
